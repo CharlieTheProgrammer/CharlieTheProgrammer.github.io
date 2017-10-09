b5660baf75ede28832eb44ee369cda9909743c53
CharlieTheProgrammer <CharlieTheProgrammer@outlook.com>
2017-10-08 20:23:35 -0700

    Updated UI and added JS functions to prepare for new features to add and remove time zones from clock.

--- ReadMe.txt
+++ ReadMe.txt
@@ -44,3 +44,25 @@ then click on one of the buttons to select
 
 
 ***** Phase III *****
+-- Storyboard --
+Adding a clock
+1. User clicks on 'Add New Time' button. Timezones appear.
+2. User clicks on a timezone, trigger a prompt "Set main clock to selected Timezone"
+  2a. If yes, then update main clock. Add current clock to the first slot.
+  2b. If no, add selected timezone to first slot.
+
+Selecting a stored clock
+1. User clicks on a saved clock, main clock is updated.
+
+Removing a stored clock
+1. User click on 'Remove TimeZones' button. Saved timezones will be highlighted.
+    1a. Hide timezones.
+2. Remove whichever saved timezones user clicks on.
+3. User must click on 'Remove TimeZones' button to de-activate 'delete mode'.
+
+
+
+
+-- To do --
+1. DONE - Add new element to engage 'delete mode'.
+2. DONE - Finalize the CSS/HTML work first, then work on the JS.
--- css_styling.js
+++ css_styling.js
@@ -1,10 +1,10 @@
 "use strict"
 
 // On Click event to hide/show the timezone selections
-// Hide/Show the available timezones when user clicks on "Select Time Zone" button.
+// Hide/Show the available timezones when user clicks on the button to add time zone.
 function hide_showAvailableTimezones() {
     //var timezones = document.getElementsByClassName('timezones');
-    var timezones = document.querySelectorAll(".timezones");
+    var timezones = document.querySelectorAll('.timezones');
 
     // if opacity is 0
     if (timezones[0].style.opacity == 0 || timezones[0].style.opacity == "") {
@@ -18,5 +18,38 @@ function hide_showAvailableTimezones() {
     }
 }
 
-var elTimezoneSelector = document.getElementById("timezone-selector");
+
+function changeSavedTimezonesTextColor() {
+    var elSavedTimeszones = document.querySelectorAll('.saved-timezones');
+    var elSavedTimeszonesDelete = document.querySelectorAll('.saved-timezones-delete');
+
+    if (elSavedTimeszones.length > 0){ // Check that elements returned
+        for (var i = 0; i < elSavedTimeszones.length; i++) {
+            elSavedTimeszones[i].setAttribute('class', 'saved-timezones-delete');
+        }
+    } else {
+    if (elSavedTimeszonesDelete.length > 0) {
+        for (var i = 0; i < elSavedTimeszonesDelete.length; i++) {
+            elSavedTimeszonesDelete[i].setAttribute('class', 'saved-timezones');
+        }
+    }
+    }
+}
+
+
+// Shows or hides timezones when the 'add new time zone' button is clicked.
+var elTimezoneSelector = document.getElementById('add-timezone');
 elTimezoneSelector.addEventListener('click', hide_showAvailableTimezones);
+
+// Hides time zones after they have been clicked.
+var elemsTimezones = document.getElementsByClassName('timezones');
+if (elemsTimezones) {
+    for (var i = 0; i < elemsTimezones.length; i++) {
+        elemsTimezones[i].addEventListener('click', hide_showAvailableTimezones)
+    }
+}
+
+// Highlight saved timezone text color when remove timezone button is clicked
+var elRemoveTimezone = document.getElementById('remove-timezone')
+elRemoveTimezone.addEventListener('click', changeSavedTimezonesTextColor)
+
