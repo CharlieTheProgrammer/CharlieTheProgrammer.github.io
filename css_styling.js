e81cd9a6de395ec02daacea79b55a3ac007e66ca
CharlieTheProgrammer <CharlieTheProgrammer@outlook.com>
2017-10-08 20:51:18 -0700

    Updated names for easier readability. Fixed a minor css issue on project landing page.

--- css_styling.js
+++ css_styling.js
@@ -4,52 +4,51 @@
 // Hide/Show the available timezones when user clicks on the button to add time zone.
 function hide_showAvailableTimezones() {
     //var timezones = document.getElementsByClassName('timezones');
-    var timezones = document.querySelectorAll('.timezones');
+    var timezones_Query = document.querySelectorAll('.timezones');
 
     // if opacity is 0
-    if (timezones[0].style.opacity == 0 || timezones[0].style.opacity == "") {
-        for (var i = 0;i < timezones.length;i++) {
-            timezones[i].style.opacity = 1;
+    if (timezones_Query[0].style.opacity == 0 || timezones_Query[0].style.opacity == "") {
+        for (var i = 0;i < timezones_Query.length;i++) {
+            timezones_Query[i].style.opacity = 1;
         }
     } else {
-         for (var i = 0;i < timezones.length;i++) {
-            timezones[i].style.opacity = 0;
+         for (var i = 0;i < timezones_Query.length;i++) {
+            timezones_Query[i].style.opacity = 0;
         }
     }
 }
 
 
 function changeSavedTimezonesTextColor() {
-    var elSavedTimeszones = document.querySelectorAll('.saved-timezones');
-    var elSavedTimeszonesDelete = document.querySelectorAll('.saved-timezones-delete');
+    var SavedTimeszones_Query = document.querySelectorAll('.saved-timezones');
+    var SavedTimeszonesDelete_Query = document.querySelectorAll('.saved-timezones-delete');
 
-    if (elSavedTimeszones.length > 0){ // Check that elements returned
-        for (var i = 0; i < elSavedTimeszones.length; i++) {
-            elSavedTimeszones[i].setAttribute('class', 'saved-timezones-delete');
+    if (SavedTimeszones_Query.length > 0){ // Check that elements returned
+        for (var i = 0; i < SavedTimeszones_Query.length; i++) {
+            SavedTimeszones_Query[i].setAttribute('class', 'saved-timezones-delete');
         }
     } else {
-    if (elSavedTimeszonesDelete.length > 0) {
-        for (var i = 0; i < elSavedTimeszonesDelete.length; i++) {
-            elSavedTimeszonesDelete[i].setAttribute('class', 'saved-timezones');
+        if (SavedTimeszonesDelete_Query.length > 0) {
+            for (var i = 0; i < SavedTimeszonesDelete_Query.length; i++) {
+                SavedTimeszonesDelete_Query[i].setAttribute('class', 'saved-timezones');
+            }
         }
-    }
     }
 }
 
-
 // Shows or hides timezones when the 'add new time zone' button is clicked.
-var elTimezoneSelector = document.getElementById('add-timezone');
-elTimezoneSelector.addEventListener('click', hide_showAvailableTimezones);
+var TimezoneSelector_Elem = document.getElementById('add-timezone');
+TimezoneSelector_Elem.addEventListener('click', hide_showAvailableTimezones);
 
 // Hides time zones after they have been clicked.
-var elemsTimezones = document.getElementsByClassName('timezones');
-if (elemsTimezones) {
-    for (var i = 0; i < elemsTimezones.length; i++) {
-        elemsTimezones[i].addEventListener('click', hide_showAvailableTimezones)
+var timezones_Elems = document.getElementsByClassName('timezones');
+if (timezones_Elems) {
+    for (var i = 0; i < timezones_Elems.length; i++) {
+        timezones_Elems[i].addEventListener('click', hide_showAvailableTimezones)
     }
 }
 
 // Highlight saved timezone text color when remove timezone button is clicked
-var elRemoveTimezone = document.getElementById('remove-timezone')
-elRemoveTimezone.addEventListener('click', changeSavedTimezonesTextColor)
+var removeTimezone_Elem = document.getElementById('remove-timezone')
+removeTimezone_Elem.addEventListener('click', changeSavedTimezonesTextColor)
 
